2b407dfa76915a8c0b7b594d2e4bdce66df48cb7
KoretskiyKirill <koretskiykirill22@gmail.com>
2025-05-27 09:56:40 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,20 @@
+#уровень C сложный
+#1
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+root = Tk()
+def down(down):
+    messagebox.showinfo("вниз", "вниз")
+def up(up):
+    messagebox.showinfo("вверх", "вверх")
 
+def left(left):
+    messagebox.showinfo("влево", "влево")
+def right(right):
+    messagebox.showinfo("вправо", "вправо")
+root.bind('<Down>', down)
+root.bind('<Up>', up)
+root.bind('<Left>', left)
+root.bind('<Right>', right)
+root.mainloop()
